c62a0475b46cde6dfff47a513126450a3d31d0f8
David Kim <woody@woody.co.kr>
2022-06-29 19:04:12 +0900

    VER 1.0.0 MLMDatabase
    VER 1.0.0 MLMDatabase

--- src/v1/Module/MLMDatabase.php
+++ src/v1/Module/MLMDatabase.php
@@ -12,7 +12,8 @@ class MLMDatabase{
     protected function Connect() : void { $this->_connect = new mysqli($this->_config->host(),$this->_config->user(),$this->_config->pass(),$this->_config->schema(),$this->_config->port()); if($this->_connect->connect_errno){ MLPError::ErrorStop(701,"[Error] Database Connect Error"); } $this->_connect->set_charset($this->_config->charset()); }
     protected function Close() : void { $this->_connect->close(); }
     public function query(string $query) : ?mysqli_result{ $this->Connect(); $res = $this->_connect->query($query); $this->Close(); if($res === false) return null; return $res; }
-    public function db() : ?mysqli { return $this->_connect ?? null; }
+    public function db() : ?mysqli { $this->Connect(); return $this->_connect ?? null; }
+    public function dbClose() : void { @$this->_connect->close(); }
     public function getArray() : array { return $this->_config->getArray() ?? array(); }
     public function getJSON() : string { return $this->_config->getJSON() ?? ""; }
 }
